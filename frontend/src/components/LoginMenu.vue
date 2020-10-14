f60f27c2b29539f4d55fd9c0e44717820c177ea4
Nev-aA <alexandre.gasior@gmail.com>
2020-10-14 17:22:36 +0200

    login & signup display done

--- frontend/src/components/LoginMenu.vue
+++ frontend/src/components/LoginMenu.vue
@@ -1,92 +1,212 @@
 <template>
-    <div class="form">
-        <form method="POST" class="form__form">
-            <img src="../assets/icon-left-font-monochrome-black.svg" alt="Groupomania logo" class="form__logo">
-            <h1 class="form__title">Connection</h1>
+    <section class="login-form-menu">
+        <div class="login-form-menu__main">
+            <form method="POST" class="login-form-menu__form">
+                <h1 class="login-form-menu__title">Connexion</h1>
 
-            <div class="form__box">
-                <span class="form__error form__error--high" v-if="requestError && submited">Votre E-mail ou votre mot de passe est incorrect</span>
-                <label for="email" class="form__label">E-mail</label>
-                <input type="text" name="email" id="email" class="form__input" placeholder="E-mail" v-model.lazy="$v.email.$model">
-                <span class="form__error" v-if="!$v.email.required && submited">Ce champ doit être complêté</span>
-                <span class="form__error" v-if="!$v.email.email">L'adresse email doit être au format email@groupomania.fr</span>
-            </div>
+                <span
+                    class="login-form-menu__form-error"
+                    v-if="requestError && submited"
+                    >Votre E-mail ou votre mot de passe est incorrect</span
+                >
+                <div class="login-form-menu__form-box">
+                    <label for="email" class="login-form-menu__form-label"
+                        >E-mail</label
+                    >
+                    <input
+                        type="text"
+                        name="email"
+                        id="email"
+                        class="login-form-menu__form-input"
+                        placeholder="E-mail"
+                        v-model.lazy="$v.email.$model"
+                    />
+                    <span
+                        class="login-form-menu__form-error"
+                        v-if="!$v.email.required && submited"
+                        >Ce champ doit être complêté</span
+                    >
+                    <span
+                        class="login-form-menu__form-error"
+                        v-if="!$v.email.email"
+                        >L'adresse email doit être au format
+                        email@groupomania.fr</span
+                    >
+                </div>
 
-            <div class="form__box">
-                <label for="password" class="form__label">Mot de passe</label>
-                <input type="password" name="password" id="password"  class="form__input" placeholder="Mot de passe" v-model.lazy="$v.password.$model">
-                <span class="form__error" v-if="!$v.password.required && submited">Ce champ doit être complêté</span>
-                <span class="form__error" v-if="!$v.password.minLength">Le mot de passe doit contenir au moins {{ $v.password.$params.minLength.min }} caractères.</span>
-            </div>
-                                
-            <button class="form__button" type="submit" @click.prevent="submitForm">Se connecter</button>
-        </form>
+                <div class="login-form-menu__form-box">
+                    <label for="password" class="login-form-menu__form-label"
+                        >Mot de passe</label
+                    >
+                    <input
+                        type="password"
+                        name="password"
+                        id="password"
+                        class="login-form-menu__form-input"
+                        placeholder="Mot de passe"
+                        v-model.lazy="$v.password.$model"
+                    />
+                    <span
+                        class="login-form-menu__form-error"
+                        v-if="!$v.password.required && submited"
+                        >Ce champ doit être complêté</span
+                    >
+                    <span
+                        class="login-form-menu__form-error"
+                        v-if="!$v.password.minLength"
+                        >Le mot de passe doit contenir au moins
+                        {{ $v.password.$params.minLength.min }}
+                        caractères.</span
+                    >
+                </div>
 
-        <p>Vous n'avez pas encore de compte ?</p>
-        <router-link class="form__router-link" to="/signup">
-            <button class="form__signup-button">S'incrire</button>
-        </router-link>
-    </div>
+                <button
+                    class="login-form-menu__form-submit-button"
+                    type="submit"
+                    @click.prevent="submitForm"
+                >
+                    Se connecter
+                </button>
+            </form>
+
+            <p class="login-form-menu__signup-paragraph">
+                Vous n'avez pas encore de compte ?
+            </p>
+            <router-link class="login-form-menu__router-link" to="/signup">
+                <button class="login-form-menu__signup-button">
+                    S'incrire
+                </button>
+            </router-link>
+        </div>
+    </section>
 </template>
 
 <script>
-import axios from 'axios'
-import { required, minLength, email} from 'vuelidate/lib/validators'
+import axios from "axios";
+import { required, minLength, email } from "vuelidate/lib/validators";
 
 export default {
-    name: 'Login',
+    name: "Login",
     data() {
-        return{
-            email: '',
-            password: '',
+        return {
+            email: "",
+            password: "",
             submited: false,
-            requestError: false
-        }
+            requestError: false,
+        };
     },
     validations: {
         email: {
             required,
-            email
+            email,
         },
         password: {
             required,
-            minLength: minLength(6)
-        }
+            minLength: minLength(6),
+        },
     },
     methods: {
         submitForm() {
-            this.submited = true;   // submited equals true once form has been submited to show errors for missing fields
-            if(!this.$v.$invalid) { // send a request to backend if form is correct to login the user
-                axios.post('http://localhost:3000/api/login',{
-                email: this.email,
-                password: this.password
-                },
-                {
-                    headers:{ 'Content-type': 'application/json'}
-                })
-                .then(res => {  // and create a sessionStorage to save userId and userToken
-                    sessionStorage.setItem('userToken', res.data.userToken);
-                    sessionStorage.setItem('userId', parseInt(res.data.userId));
-                    window.location.href="/";
-                })
-                .catch(error => {
-                    this.requestError = true;
-                    console.log({error})
-                });
+            this.submited = true; // submited equals true once form has been submited to show errors for missing fields
+            if (!this.$v.$invalid) {
+                // send a request to backend if form is correct to login the user
+                axios
+                    .post(
+                        "http://localhost:3000/api/login",
+                        {
+                            email: this.email,
+                            password: this.password,
+                        },
+                        {
+                            headers: { "Content-type": "application/json" },
+                        }
+                    )
+                    .then((res) => {
+                        // and create a sessionStorage to save userId and userToken
+                        sessionStorage.setItem("userToken", res.data.userToken);
+                        sessionStorage.setItem(
+                            "userId",
+                            parseInt(res.data.userId)
+                        );
+                        window.location.href = "/";
+                    })
+                    .catch((error) => {
+                        this.requestError = true;
+                        console.log({ error });
+                    });
             }
-        }
-    }
-}
+        },
+    },
+};
 </script>
 
 <style lang="scss">
-.form__logo {
-    max-height: 250px;
-}
+.login-form-menu {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
 
-.form__error {
-  color: red;
-  font-size: 12px;
-  text-transform: uppercase;
+    &__main {
+        border-radius: 30px;
+        box-shadow: 0px 0px 10px 5px rgba(184, 184, 184, 0.7);
+        margin: 20px auto;
+        background-color: #192946;
+        max-width: 1000px;
+        width: 90%;
+    }
+
+    &__title {
+        text-transform: uppercase;
+    }
+
+    &__form {
+        width: 90%;
+        margin: 0 auto 30px auto;
+    }
+
+    &__form-label {
+        font-size: 18px;
+        font-weight: 200;
+    }
+
+    &__form-input {
+        width: 80%;
+        padding: 12px 20px;
+        margin: 8px 0;
+        box-sizing: border-box;
+    }
+
+    &__form-error {
+        color: #d1515a;
+    }
+
+    &__form-submit-button {
+        background-color: #d1515a;
+        color: #ffffff;
+        border: none;
+        border-radius: 15px;
+        padding: 15px 30px;
+        cursor: pointer;
+        &:hover {
+            opacity: 0.7;
+        }
+    }
+
+    &__signup-paragraph {
+        font-style: italic;
+    }
+
+    &__signup-button {
+        background-color: #fff;
+        color: #192946;
+        border: none;
+        border-radius: 15px;
+        padding: 10px 30px;
+        margin-bottom: 15px;
+        cursor: pointer;
+        &:hover {
+            background-color: #d1515a;
+        }
+    }
 }
 </style>
--- frontend/src/components/SignupMenu.vue
+++ frontend/src/components/SignupMenu.vue
@@ -1,133 +1,303 @@
 <template>
-    <div class="form">
-        <form method="POST" class="form__form">
-            <img src="../assets/icon-left-font-monochrome-black.svg" alt="Groupomania logo" class="form__logo">
-            <h1 class="form__title">Inscription</h1>
+    <section class="signup-form-menu">
+        <div class="signup-form-menu__main">
+            <form method="POST" class="signup-form-menu__form">
+                <h1 class="signup-form-menu__title">Inscription</h1>
 
-            <div class="form__box">
-                <label for="firstName" class="form__label">Prénom</label>
-                <input type="text" name="firstName" id="firstName" class="form__input" placeholder="Prénom" v-model.lazy="$v.firstName.$model">
-                <span class="form__error" v-if="!$v.firstName.required && submited">Ce champ doit être complêté.</span>
-                <span class="form__error" v-if="!$v.firstName.alpha">Ce champ ne peut contenir que des lettres.</span>
-                <span class="form__error" v-if="!$v.firstName.minLength">Le prénom doit contenir au moins {{ $v.firstName.$params.minLength.min }} lettres.</span>
-            </div>
+                <div class="signup-form-menu__form-box">
+                    <label for="firstName" class="signup-form-menu__form-label"
+                        >Prénom</label
+                    >
+                    <input
+                        type="text"
+                        name="firstName"
+                        class="signup-form-menu__form-input"
+                        placeholder="Prénom"
+                        v-model.lazy="$v.firstName.$model"
+                    />
+                    <span
+                        class="signup-form-menu__form-error"
+                        v-if="!$v.firstName.required && submited"
+                        >Ce champ doit être complêté.</span
+                    >
+                    <span
+                        class="signup-form-menu__form-error"
+                        v-if="!$v.firstName.alpha"
+                        >Ce champ ne peut contenir que des lettres.</span
+                    >
+                    <span
+                        class="signup-form-menu__form-error"
+                        v-if="!$v.firstName.minLength"
+                        >Le prénom doit contenir au moins
+                        {{ $v.firstName.$params.minLength.min }} lettres.</span
+                    >
+                </div>
 
-            <div class="form__box">
-                <label for="lastName" class="form__label">Nom</label>
-                <input type="text" name="lastName" id="lastName" class="form__input" placeholder="Nom" v-model.lazy="$v.lastName.$model">
-                <span class="form__error" v-if="!$v.lastName.required && submited">Ce champ doit être complêté</span>
-                <span class="form__error" v-if="!$v.lastName.alpha">Ce champ ne peut contenir que des lettres.</span>
-                <span class="form__error" v-if="!$v.lastName.minLength">Le Nom doit contenir au moins {{ $v.lastName.$params.minLength.min }} lettres.</span>
-            </div>
+                <div class="signup-form-menu__form-box">
+                    <label for="lastName" class="form__form-label">Nom</label>
+                    <input
+                        type="text"
+                        name="lastName"
+                        class="signup-form-menu__form-input"
+                        placeholder="Nom"
+                        v-model.lazy="$v.lastName.$model"
+                    />
+                    <span
+                        class="signup-form-menu__form-error"
+                        v-if="!$v.lastName.required && submited"
+                        >Ce champ doit être complêté</span
+                    >
+                    <span
+                        class="signup-form-menu__form-error"
+                        v-if="!$v.lastName.alpha"
+                        >Ce champ ne peut contenir que des lettres.</span
+                    >
+                    <span
+                        class="signup-form-menu__form-error"
+                        v-if="!$v.lastName.minLength"
+                        >Le Nom doit contenir au moins
+                        {{ $v.lastName.$params.minLength.min }} lettres.</span
+                    >
+                </div>
 
-            <div class="form__box">
-                <label for="email" class="form__label">E-mail</label>
-                <input type="text" name="email" id="email" class="form__input" placeholder="E-mail" v-model.lazy="$v.email.$model">
-                <span class="form__error" v-if="!$v.email.required && submited">Ce champ doit être complêté</span>
-                <span class="form__error" v-if="!$v.email.email">L'adresse email doit être au format email@groupomania.fr</span>
-                <span class="form__error" v-if="emailAlreadyTaken">Cette adresse email est déjà prise</span>
-            </div>
+                <div class="signup-form-menu__form-box">
+                    <label for="email" class="signup-form-menu__form-label"
+                        >E-mail</label
+                    >
+                    <input
+                        type="text"
+                        name="email"
+                        class="signup-form-menu__form-input"
+                        placeholder="E-mail"
+                        v-model.lazy="$v.email.$model"
+                    />
+                    <span
+                        class="signup-form-menu__form-error"
+                        v-if="!$v.email.required && submited"
+                        >Ce champ doit être complêté</span
+                    >
+                    <span
+                        class="signup-form-menu__form-error"
+                        v-if="!$v.email.email"
+                        >L'adresse email doit être au format
+                        email@groupomania.fr</span
+                    >
+                    <span
+                        class="signup-form-menu__form-error"
+                        v-if="emailAlreadyTaken"
+                        >Cette adresse email est déjà prise</span
+                    >
+                </div>
 
-            <div class="form__box">
-                <label for="password" class="form__label">Mot de passe</label>
-                <input type="password" name="password" id="password"  class="form__input" placeholder="Mot de passe" v-model.lazy="$v.password.$model">
-                <span class="form__error" v-if="!$v.password.required && submited">Ce champ doit être complêté</span>
-                <span class="form__error" v-if="!$v.password.minLength">Le mot de passe doit contenir au moins {{ $v.password.$params.minLength.min }} caractères.</span>
-            </div>
-                                
-            <button class="form__button" type="submit" @click.prevent="submitForm">S'inscrire</button>
-        </form>
+                <div class="signup-form-menu__form-box">
+                    <label for="password" class="signup-form-menu__form-label"
+                        >Mot de passe</label
+                    >
+                    <input
+                        type="password"
+                        name="password"
+                        id="password"
+                        class="signup-form-menu__form-input"
+                        placeholder="Mot de passe"
+                        v-model.lazy="$v.password.$model"
+                    />
+                    <span
+                        class="signup-form-menu__form-error"
+                        v-if="!$v.password.required && submited"
+                        >Ce champ doit être complêté</span
+                    >
+                    <span
+                        class="signup-form-menu__form-error"
+                        v-if="!$v.password.minLength"
+                        >Le mot de passe doit contenir au moins
+                        {{ $v.password.$params.minLength.min }}
+                        caractères.</span
+                    >
+                </div>
 
-        <p>Vous avez déjà un compte ?</p>
-        <router-link class="form__router-link" to="/login">
-            <button class="form__login-button">Se connecter</button>
-        </router-link>
-    </div>
+                <button
+                    class="signup-form-menu__form-submit-button"
+                    type="submit"
+                    @click.prevent="submitForm"
+                >
+                    S'inscrire
+                </button>
+            </form>
+
+            <p class="signup-form-menu__login-paragraph">
+                Vous avez déjà un compte ?
+            </p>
+            <router-link class="signup-form-menu__router-link" to="/login">
+                <button class="signup-form-menu__login-button">
+                    Se connecter
+                </button>
+            </router-link>
+        </div>
+    </section>
 </template>
 
 <script>
-import axios from 'axios'
-import { required, minLength, alpha, email} from 'vuelidate/lib/validators'
+import axios from "axios";
+import { required, minLength, alpha, email } from "vuelidate/lib/validators";
 export default {
-    name: 'Signup',
+    name: "Signup",
     data() {
-        return{
-            firstName: '',
-            lastName: '',
-            email: '',
-            password: '',
+        return {
+            firstName: "",
+            lastName: "",
+            email: "",
+            password: "",
             submited: false,
-            emailAlreadyTaken: false
-        }
+            emailAlreadyTaken: false,
+        };
     },
     validations: {
         firstName: {
             required,
             alpha,
-            minLength: minLength(2)
+            minLength: minLength(2),
         },
         lastName: {
             required,
             alpha,
-            minLength: minLength(2)
+            minLength: minLength(2),
         },
         email: {
             required,
-            email
+            email,
         },
         password: {
             required,
             minLength: minLength(6),
-        }
+        },
     },
     methods: {
         submitForm() {
-            this.submited = true;   // submited equals true once form has been submited to show errors for missing fields
-            if(!this.$v.$invalid) { // send a request post to backend if form is correct
-                axios.post('http://localhost:3000/api/signup',{
-                    firstName: this.firstName,
-                    lastName: this.lastName,
-                    email: this.email,
-                    password: this.password
-                },
-                {
-                    headers:{ 'Content-type': 'application/json'}
-                })
-                .then(() => {       // then login the user created
-                    axios.post('http://localhost:3000/api/login',{
-                    email: this.email,
-                    password: this.password
-                    },
-                    {
-                        headers:{ 'Content-type': 'application/json'}
+            this.submited = true; // submited equals true once form has been submited to show errors for missing fields
+            if (!this.$v.$invalid) {
+                // send a request post to backend if form is correct
+                axios
+                    .post(
+                        "http://localhost:3000/api/signup",
+                        {
+                            firstName: this.firstName,
+                            lastName: this.lastName,
+                            email: this.email,
+                            password: this.password,
+                        },
+                        {
+                            headers: { "Content-type": "application/json" },
+                        }
+                    )
+                    .then(() => {
+                        // then login the user created
+                        axios
+                            .post(
+                                "http://localhost:3000/api/login",
+                                {
+                                    email: this.email,
+                                    password: this.password,
+                                },
+                                {
+                                    headers: {
+                                        "Content-type": "application/json",
+                                    },
+                                }
+                            )
+                            .then((res) => {
+                                // and create a sessionStorage to save userId and userToken
+                                sessionStorage.setItem(
+                                    "userToken",
+                                    res.data.userToken
+                                );
+                                sessionStorage.setItem(
+                                    "userId",
+                                    parseInt(res.data.userId)
+                                );
+                                window.location.href = "/";
+                            })
+                            .catch((error) => {
+                                console.log({ error });
+                            });
                     })
-                    .then(res => {  // and create a sessionStorage to save userId and userToken
-                        sessionStorage.setItem('userToken', res.data.userToken);
-                        sessionStorage.setItem('userId', parseInt(res.data.userId));
-                        window.location.href="/";
-                    })
-                    .catch(error => {
-                        console.log({error})
+                    .catch((error) => {
+                        // in case email has already been taken change emailAlreadyTaken to true to indicate it to the user
+                        this.emailAlreadyTaken = true;
+                        console.log({ error });
                     });
-                })
-                .catch(error => {   // in case email has already been taken change emailAlreadyTaken to true to indicate it to the user
-                    this.emailAlreadyTaken = true;
-                    console.log({error})
-                });
             }
-        }
-    }
-}
+        },
+    },
+};
 </script>
 
 <style lang="scss">
-.form__logo {
-    max-height: 250px;
-}
+.signup-form-menu {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
 
-.form__error {
-  color: red;
-  font-size: 12px;
-  text-transform: uppercase;
+    &__main {
+        border-radius: 30px;
+        box-shadow: 0px 0px 10px 5px rgba(184, 184, 184, 0.7);
+        margin: 20px auto;
+        background-color: #192946;
+        max-width: 1000px;
+        width: 90%;
+    }
+
+    &__title {
+        text-transform: uppercase;
+    }
+
+    &__form {
+        width: 90%;
+        margin: 0 auto 30px auto;
+    }
+
+    &__form-label {
+        font-size: 18px;
+        font-weight: 200;
+    }
+
+    &__form-input {
+        width: 80%;
+        padding: 12px 20px;
+        margin: 8px 0;
+        box-sizing: border-box;
+    }
+
+    &__form-error {
+        color: #d1515a;
+    }
+
+    &__form-submit-button {
+        background-color: #d1515a;
+        color: #ffffff;
+        border: none;
+        border-radius: 15px;
+        padding: 15px 30px;
+        cursor: pointer;
+        &:hover {
+            opacity: 0.7;
+        }
+    }
+
+    &__login-paragraph {
+        font-style: italic;
+    }
+
+    &__login-button {
+        background-color: #fff;
+        color: #192946;
+        border: none;
+        border-radius: 15px;
+        padding: 10px 30px;
+        margin-bottom: 15px;
+        cursor: pointer;
+        &:hover {
+            background-color: #d1515a;
+        }
+    }
 }
 </style>
