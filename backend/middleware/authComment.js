7d1350db42a7d5f449ecc5a6f10ed3b59a1c2a6b
Nev-aA <alexandre.gasior@gmail.com>
2020-10-18 17:59:10 +0200

    add a middleware to authorize delete and modify for comments only for user whom created the comment or admin

--- backend/middleware/authComment.js
+++ backend/middleware/authComment.js
@@ -1,23 +1,23 @@
 require('dotenv').config();
 const jwt = require('jsonwebtoken');
-const Post = require('../models/Post');
+const Comment = require('../models/Comment');
 
 module.exports = (req, res, next) => {
     const token = req.headers.authorization.split(' ')[1];
     const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN);
     const userRole = decodedToken.userRole;
     const userId = decodedToken.userId;
-    Post.findOne({
+    Comment.findOne({
         where: {
             id: req.params.id
         }
     })
-    .then(post => {
-        if((post.userId === userId) || userRole === 'admin'){
+    .then(comment => {
+        if((comment.userId === userId) || userRole === 'admin'){
             next();
         }
         else{
-            res.status(401).json({message: "Action non autorisée"})
+            res.status(401).json({message: 'Action non autorisée'})
         }
     })
     .catch(error => res.status(500).json({error}))
--- backend/routes/comment.js
+++ backend/routes/comment.js
@@ -2,10 +2,11 @@ const express = require('express');
 const router = express.Router();
 const commentCtrl = require('../controllers/comment');
 const auth = require('../middleware/auth');
+const authComment = require('../middleware/authComment');
 
 router.post('/create/:id', auth, commentCtrl.createComment);
 router.get('/:id', auth, commentCtrl.getCommentsfromPost);
-router.delete('/:id', auth, commentCtrl.deleteComment);
-router.post('/:id', auth, commentCtrl.modifyComment);
+router.delete('/:id', auth, authComment, commentCtrl.deleteComment);
+router.post('/:id', auth, authComment, commentCtrl.modifyComment);
 
 module.exports = router;
